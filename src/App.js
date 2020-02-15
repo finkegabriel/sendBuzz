bc68fd2e0d375c3ba535352d3dbd172254af6d42
Gabe <finkegabriel@gmail.com>
2020-02-15 16:05:39 -0700

    more buzz

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './App.css';
 
+var vibrateInterval;
+
+// Starts vibration at passed in level
+function start(){
+  startPersistentVibrate(100,10);
+}
+
+function startVibrate(duration) {
+  navigator.vibrate(duration);
+}
+
+// Stops vibration
+function stopVibrate() {
+  // Clear interval and stop persistent vibrating
+  if (vibrateInterval) clearInterval(vibrateInterval);
+  navigator.vibrate(0);
+}
+
+// Start persistent vibration at given duration and interval
+// Assumes a number value is given
+function startPersistentVibrate(duration, interval) {
+  console.log("BUZZZzzZZZzzzzzzzzzzz");
+  vibrateInterval = setInterval(function () {
+    startVibrate(duration);
+  }, interval);
+}
 function App() {
-  var vibrateInterval;
-
-  // Starts vibration at passed in level
-  function start(){
-    startPersistentVibrate(100,10);
-  }
-
-  function startVibrate(duration) {
-    navigator.vibrate(duration);
-  }
-
-  // Stops vibration
-  function stopVibrate() {
-    // Clear interval and stop persistent vibrating
-    if (vibrateInterval) clearInterval(vibrateInterval);
-    navigator.vibrate(0);
-  }
-
-  // Start persistent vibration at given duration and interval
-  // Assumes a number value is given
-  function startPersistentVibrate(duration, interval) {
-    console.log("BUZZZzzZZZzzzzzzzzzzz");
-    vibrateInterval = setInterval(function () {
-      startVibrate(duration);
-    }, interval);
-  }
 
   return (
     <div className="App">
